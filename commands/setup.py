4e568c094e89ba6eaeb6cd574e19b88ec6a96fc8
Edmund Lam <edlam1889@gmail.com>
2018-04-24 08:03:39 -0700

    update

--- commands/setup.py
+++ commands/setup.py
@@ -1,9 +1,13 @@
 import asyncio
 from demobot.utils import *
-from demobot.handlers import add_message_handler
+from demobot.handlers import add_message_handler, server_data
 from discord import Embed
 
 @asyncio.coroutine
-def role(Demobot, msg):
-    yield from Demobot.send_message(msg.channel, "hi")
-add_message_handler(role, r'make\s*(<@&[0-9]*>|@everyone)\s*a\s*(leader|president|judge|enforcer|rep(?:resentative)?)\s*role')
+def role(Demobot, msg, reg):
+    perms = msg.channel.permissions_for(msg.author)
+    if perms.manage_server:
+        if msg.server.id not in server_data:
+            server_data[msg.server.id] = 
+        yield from Demobot.send_message(msg.channel, reg.group('name'))
+add_message_handler(role, r'make\s*(<@&[0-9]*>|@everyone)\s*a\s*(?P<name>leader|president|judge|enforcer|rep(?:resentative)?)\s*role')
--- demobot/handlers.py
+++ demobot/handlers.py
@@ -11,17 +11,35 @@ message_handlers = {}
 private_message_handlers = {}
 
 print("\tBegin Loading Files")
-closing = False
+
 
 
 print("\tLoaded files")
 
 persistent_variables = {}
 
+if not os.path.exists("data/data_backup/"):
+    os.makedirs("data/data_backup/")
+
+server_data = {}
+if os.path.isfile("data/settings.txt"):
+    with open("data/settings.txt", "rb") as f:
+        server_data = pickle.load(f)
+    copyfile("data/settings.txt", "data/data_backup/settings.txt")
+
 def add_message_handler(handler, keyword):
     message_handlers[keyword] = handler
 def add_private_message_handler(handler, keyword):
     private_message_handlers[keyword] = handler
+def update_user_data(channelid, obj, val, addon=False):
+    if channelid in server_data:
+        if addon:
+            if obj in server_data[channelid]:
+                server_data[channelid][obj] += val
+                return
+        server_data[channelid][obj] = val
+    else:
+        server_data[channelid] = {obj:val}
 
 print("Handler initialized")
 print("Begin Command Initialization")
@@ -42,17 +60,18 @@ def on_message(Demobot, msg):
             if msg.channel.is_private:
                 yield from Demobot.send_message(msg.channel, "Demobot doesn't work in private channels")
             for a in message_handlers:
-                if re.compile(a, re.I).match(msg.content):
-                    yield from Demobot.send_message(msg.channel, '`'+msg.content+'`')
+                reg = re.compile(a, re.I).search(msg.content)
+                if reg:
+                    yield from message_handlers[a](Demobot, msg, reg)
         except IndexError:
-            em = discord.Embed(title="Missing Inputs", description="Not enough inputs provided for **%s**." % parse_command(msg.content)[0], colour=0xd32323)
+            em = discord.Embed(title="Missing Inputs", description="Not enough inputs provided.", colour=0xd32323)
             yield from send_embed(Demobot, msg, em)
         except (TypeError, ValueError):
-            em = discord.Embed(title="Invalid Inputs", description="Invalid inputs provided for **%s**." % parse_command(msg.content)[0], colour=0xd32323)
+            em = discord.Embed(title="Invalid Inputs", description="Invalid inputs provided.", colour=0xd32323)
             yield from send_embed(Demobot, msg, em)
         except discord.Forbidden:
             em = discord.Embed(title="Missing Permissions", description="Demobot is missing permissions to perform this task.", colour=0xd32323)
             yield from send_embed(Demobot, msg, em)
         except Exception as e:
-            em = discord.Embed(title="Unknown Error", description="An unknown error occurred. Trace:\n%s" % (parse_command(msg.content)[0], e), colour=0xd32323)
+            em = discord.Embed(title="Unknown Error", description="An unknown error occurred. Trace:\n%s" % e, colour=0xd32323)
             yield from send_embed(Demobot, msg, em)
