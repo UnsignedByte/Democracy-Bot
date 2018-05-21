19928b92ccf132b2c68e9e852890c8348d95a9cd
Edmund Lam <edlam1889@gmail.com>
2018-05-21 07:53:31 -0700

    tmp up

--- commands/proposals.py
+++ commands/proposals.py
@@ -1,6 +1,7 @@
 import asyncio
 from demobot.utils import *
-from demobot.handlers import add_message_handler, nested_get
+from demobot.handlers import add_message_handler, nested_get, nested_append, nested_set
+from commands.utilities import save
 from discord import Embed
 
 async def propose(Demobot, msg, reg):
@@ -19,9 +20,18 @@ async def propose(Demobot, msg, reg):
         else:
             type = "rule"
         propchan = nested_get(msg.server.id, "channels", "proposals")
-        title = reg.group("title").title()
+        title = reg.group("title")
         if not title:
             title = "Untitled Proposal"
-        await Demobot.send_message(propchan, '%s %s Proposal:\n\n**%s**\nID: %s\n\n%s' % (nested_get(msg.server.id, "roles", "representative").mention, type, title, msg.id, reg.group("content")))
+        else:
+            title = title.title()
+        newm = await Demobot.send_message(propchan, '%s %s Proposal:\n\n**%s**\nID: %s\n\n%s' % (nested_get(msg.server.id, "roles", "representative").mention, type, title, msg.id, reg.group("content")))
+        await Demobot.add_reaction(newm, "👍")
+        await Demobot.add_reaction(newm, "👎")
+        await Demobot.add_reaction(newm, "➖")
+        nested_append(newm, msg.server.id, "proposals", "messages")
+        await save(None, None, None, overrideperms=True)
 
-add_message_handler(propose, r'<@&(?P<roleid>[0-9]*)>\s*(?P<type>.*?)\s*proposal:\n*\*\*(?P<title>.*?)?\*\*\n*(?P<content>(?:.|\n)*?)\Z')
+
+
+add_message_handler(propose, r'<@&(?P<roleid>[0-9]*)>\s*(?P<type>.*?)\s*proposal:\n*(?:\*\*(?P<title>.*?)\*\*)?\n*(?P<content>(?:.|\n)*?)\Z')
--- demobot/handlers.py
+++ demobot/handlers.py
@@ -46,12 +46,29 @@ def nested_set(value, *keys):
         dic = dic.setdefault(key, {})
     dic[keys[-1]] = value
 
-
 def nested_get(*keys):
     dic = server_data
     for key in keys:
         dic=dic.setdefault( key, {} )
     return dic
+
+def nested_append(value, *keys):
+    v = nested_get(*keys)
+    if v:
+        v.append(value)
+    else:
+        nested_set([value], *keys)
+
+def nested_remove(value, *keys, **kwargs):
+    kwargs['func'] = kwargs.get('func', None)
+    v = nested_get(*keys)
+    if not kwargs['func']:
+        v.remove(value)
+    else:
+        for x in v:
+            if kwargs['func'](x, value):
+                v.remove(x)
+                break
 
 
 print("Handler initialized")
@@ -95,29 +112,36 @@ async def on_message(Demobot, msg):
             await send_embed(Demobot, msg, em)
 
 
-async def timed_message(Demobot):
-    currt = datetime.now(tz=pytz.utc)
-    nextelection = currt + timedelta( (2-currt.weekday()) % 7 + 1 )
-    nextelection = nextelection.replace(hour=8, minute=0, second=0, microsecond=0)
-    await asyncio.sleep((nextelection-currt).total_seconds())
-    for a in server_data:
-        chann = nested_get(a, "channels", "announcements")
-        if chann:
-            await Demobot.send_message(chann, "Hey "+nested_get(a, "roles", "citizen").mention+"! You will be able to run for positions in government later today at "+(nextelection+timedelta(hours=12)).astimezone(pytz.timezone('US/Pacific')).strftime('%H:%M:%S')+" PST.")
-    await asyncio.sleep(43200)
-    for a in server_data:
-        chann = nested_get(a, "channels", "announcements")
-        if chann:
-            electionmsg = await Demobot.send_message(chann, "Hey "+nested_get(a, "roles", "citizen").mention+"! You may now run for positions in government!\nTo do so, type `I am running for (position)` (remove the parentheses).\nElections will start later today at "+(nextelection+timedelta(hours=18)).astimezone(pytz.timezone('US/Pacific')).strftime('%H:%M:%S')+" PST.")
-        nested_set(electionmsg, a, "elections", "runnable")
-    await asyncio.sleep(21600)
-    nested_set(None, a, "elections", "runnable")
-    for a in server_data:
-        chann = nested_get(a, "channels", "announcements")
-        if chann:
-            electionmsg = await Demobot.send_message(chann, "Hey "+nested_get(a, "roles", "citizen").mention+"! Elections have now started. They will last until tomorrow at "+(nextelection+timedelta(hours=18)).astimezone(pytz.timezone('US/Pacific')).strftime('%H:%M:%S')+" PST.")
-        nested_set(electionmsg, a, "elections", "election")
-    await asyncio.sleep(86400)
+async def elections_timed(Demobot):
+    while True:
+        currt = datetime.now(tz=pytz.utc)
+        nextelection = currt + timedelta( (2-currt.weekday()) % 7 + 1 )
+        nextelection = nextelection.replace(hour=8, minute=0, second=0, microsecond=0)
+        await asyncio.sleep((nextelection-currt).total_seconds())
+        for a in server_data:
+            chann = nested_get(a, "channels", "announcements")
+            if chann:
+                await Demobot.send_message(chann, "Hey "+nested_get(a, "roles", "citizen").mention+"! You will be able to run for positions in government later today at "+(nextelection+timedelta(hours=12)).astimezone(pytz.timezone('US/Pacific')).strftime('%H:%M:%S')+" PST.")
+        await asyncio.sleep(43200)
+        for a in server_data:
+            chann = nested_get(a, "channels", "announcements")
+            if chann:
+                electionmsg = await Demobot.send_message(chann, "Hey "+nested_get(a, "roles", "citizen").mention+"! You may now run for positions in government!\nTo do so, type `I am running for (position)` (remove the parentheses).\nElections will start later today at "+(nextelection+timedelta(hours=18)).astimezone(pytz.timezone('US/Pacific')).strftime('%H:%M:%S')+" PST.")
+            nested_set(electionmsg, a, "elections", "runnable")
+        await asyncio.sleep(21600)
+        nested_set(None, a, "elections", "runnable")
+        for a in server_data:
+            chann = nested_get(a, "channels", "announcements")
+            if chann:
+                electionmsg = await Demobot.send_message(chann, "Hey "+nested_get(a, "roles", "citizen").mention+"! Elections have now started. They will last until tomorrow at "+(nextelection+timedelta(hours=18)).astimezone(pytz.timezone('US/Pacific')).strftime('%H:%M:%S')+" PST.")
+            nested_set(electionmsg, a, "elections", "election")
+        await asyncio.sleep(86400)
+
+async def minutely_check(Demobot):
+    while True:
+        await utilities.save(None, None, None, overrideperms=True)
+        await asyncio.sleep(60)
+
 
 async def member_update(Demobot, before, after):
     nested_set(after, after.server.id, "members", after.id)
@@ -125,4 +149,10 @@ async def newuser(Demobot, user):
     oldusr = nested_get(user.server.id, "members", user.id)
     await Demobot.add_roles(user, *oldusr.roles)
     await Demobot.change_nickname(user, oldusr.nick)
-    await utilities.save(Demobot, None, None, overrideperms=True)
+    await utilities.save(None, None, None, overrideperms=True)
+async def on_reaction_add(Demobot, reaction, user):
+    msg = reaction.message
+    if msg in nested_get(msg.server.id, "proposals", "messages"):
+        pass
+async def on_reaction_delete(Demobot, reaction, user):
+    print("OOF!")
